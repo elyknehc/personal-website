7e8fda34507ee19663c9151fa057bfd7855d488d
elyknehc <kchen727@uwo.ca>
2024-08-24 22:15:49 -0400

    Fix footer error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
 		<footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
 			<hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
 			<div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
-				<div className="text-4xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold mb-4">
+				<div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
 					© 2024 Kyle Chen<a href="/" className="hover:underline"></a>
 					<div> | Last Updated: August 2024</div>
 				</div>
