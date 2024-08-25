4442df21562af23ff21a9ac2f4ef11ac173932ce
elyknehc <kchen727@uwo.ca>
2024-08-24 23:32:18 -0400

    Fix build issue

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import SlideUp from "./SlideUp";
 import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
 
 const projects = [
