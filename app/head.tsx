567d77d5e34447e5e61d003adde83bf428f0900f
elyknehc <kchen727@uwo.ca>
2024-08-26 00:47:05 -0400

    change favicon

--- app/head.tsx
+++ app/head.tsx
@@ -6,7 +6,7 @@ export default function Head() {
 			<meta name="description" content="Kyle Chen's personal website." />
 			<link
 				rel="icon"
-				href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>K</text></svg>"
+				href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22middle%22 text-anchor=%22middle%22 font-size=%2270%22>KC</text></svg>"
 			/>
 		</>
 	);
